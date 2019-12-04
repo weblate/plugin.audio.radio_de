86b567692b39173dc1ed5915d6a9cf82d51e430d
enen92 <92enen@gmail.com>
2019-12-04 11:38:58 +0000

    Update print statements

--- resources/lib/test.py
+++ resources/lib/test.py
@@ -8,11 +8,11 @@ STATION_IDS = (2279, 3643)
 def do_nothing(*args, **kwargs):
     pass
 
-print 'Get Instance'
+print('Get Instance')
 ra = RadioApi()
 ra.log = do_nothing
 
-print 'Checking get_category_types'
+print('Checking get_category_types')
 cat_types = ra.get_category_types()
 assert cat_types
 
@@ -26,21 +26,21 @@ for cat_type in cat_types:
         stations = ra.get_stations_by_category(cat_type, category)
         assert stations
 
-print 'Checking get_recommendation_stations'
+print('Checking get_recommendation_stations')
 assert ra.get_recommendation_stations()
 
-print 'Checking get_top_stations'
+print('Checking get_top_stations')
 assert ra.get_top_stations()
 
-print 'Checking get_local_stations'
+print('Checking get_local_stations')
 assert ra.get_local_stations()
 
-print 'Checking _get_most_wanted'
+print('Checking _get_most_wanted')
 assert ra._get_most_wanted()
 
-print 'Checking search_stations_by_string'
+print('Checking search_stations_by_string')
 assert ra.search_stations_by_string('WDR')
 
 for station in STATION_IDS:
-    print 'Checking get_station_by_station_id'
+    print('Checking get_station_by_station_id')
     assert ra.get_station_by_station_id(station)
